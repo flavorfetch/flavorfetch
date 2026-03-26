f74abb4d5960a60d9365c877be2ede69b7fb8cb9
Flavor <flavorfetch@gmail.com>
2026-03-26 12:52:28 +0530

    feat: integrate Firebase Cloud Messaging and order status endpoints

--- models/User.js
+++ models/User.js
@@ -37,6 +37,10 @@ const userSchema = mongoose.Schema({
     address: {
         type: String,
         default: "" 
+    },
+    fcmToken: {
+        type: String,
+        default: ""
     }
 }, { timestamps: true });
 
